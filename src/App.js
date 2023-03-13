8f4998d94d012c560f1d5933a3d04ad8c43f9074
Samudinov Rysiua <sadikat47@gmail.com>
2023-03-13 12:53:24 +0600

    lesson 7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyComponent extends Component {
+  state = {
+    names: ['Арген', 'Максат', 'Жанылай']
+  }
+
+  render() {
+    const { names } = this.state;
+
+    return (
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-export default App;
+export default MyComponent;
